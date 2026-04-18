42d23eeab3d40666ac3aec2190a79c6c1ea78d0b
Shaji Mohammed <127638929+Shaji-Mohammed@users.noreply.github.com>
2026-04-18 19:12:42 -0300

    Add: partition-labels - submission-13

---
+++ Data Structures & Algorithms/partition-labels/submission-13.py
@@ -0,0 +1,16 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        counts = {}
+        res = []
+
+        for i, v in enumerate(s):
+            counts[v] = max(i, counts.get(v, 0))
+        
+        counter = lastIndex = 0
+        for i, v in enumerate(s):
+            lastIndex = max(counts[v], lastIndex)
+            counter+=1
+            if i == lastIndex:
+                res.append(counter)
+                counter = 0
+        return res
