ed11c4fba7e208a2530a39f0a20f7b20ad9342c5
Shaji Mohammed <127638929+Shaji-Mohammed@users.noreply.github.com>
2026-09-14 17:00:16 -0400

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        sMap = {}
+        tMap = {}
+
+        for i in range(len(s)):
+            si = s[i]
+            sMap[si] = 1 + sMap.get(si, 0)
+        
+        for i in range(len(t)):
+            ti = t[i]
+            tMap[ti] = 1 + tMap.get(ti, 0)
+        
+        return True if sMap == tMap else False
